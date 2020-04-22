1e0d438f91f09c6fc0311116fecc3d8fd60c001a
Hakim El Hattab <hakim.elhattab@gmail.com>
2020-04-22 15:14:04 +0200

    move plugin out of reveal.js core

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# notes-server
-The reveal.js Speaker Notes Server Plugin
+# reveal.js Speaker Notes Server
+
+In some cases it can be desirable to run notes on a separate device from the one you're presenting on. The Node.js-based notes plugin lets you do this using the same note definitions as its client side counterpart. Include the required scripts by adding the following dependencies:
+
+```javascript
+Reveal.initialize({
+  dependencies: [
+    { src: 'socket.io/socket.io.js', async: true },
+    { src: 'plugin/notes-server/client.js', async: true }
+  ]
+});
+```
+
+Then:
+
+1. Install [Node.js](http://nodejs.org/) (9.0.0 or later)
+2. Run `npm install`
+3. Run `node plugin/notes-server`
---
+++ index.js
@@ -0,0 +1,66 @@
+let http      = require('http');
+let express   = require('express');
+let fs        = require('fs');
+let io        = require('socket.io');
+let mustache  = require('mustache');
+
+let app       = express();
+let server    = http.createServer(app);
+
+io = io(server);
+
+let opts = {
+	port: process.env.PORT || 1947,
+	baseDir : process.cwd()
+};
+
+io.on( 'connection', function( socket ) {
+
+	socket.on( 'new-subscriber', function( data ) {
+		socket.broadcast.emit( 'new-subscriber', data );
+	});
+
+	socket.on( 'statechanged', function( data ) {
+		delete data.state.overview;
+		socket.broadcast.emit( 'statechanged', data );
+	});
+
+	socket.on( 'statechanged-speaker', function( data ) {
+		delete data.state.overview;
+		socket.broadcast.emit( 'statechanged-speaker', data );
+	});
+
+});
+
+app.use( express.static( opts.baseDir ) );
+
+app.get('/', function( req, res ) {
+
+	res.writeHead( 200, { 'Content-Type': 'text/html' } );
+	fs.createReadStream( opts.baseDir + '/index.html' ).pipe( res );
+
+});
+
+app.get( '/notes/:socketId', function( req, res ) {
+
+	fs.readFile( opts.baseDir + 'plugin/notes-server/notes.html', function( err, data ) {
+		res.send( mustache.to_html( data.toString(), {
+			socketId : req.params.socketId
+		}));
+	});
+
+});
+
+// Actually listen
+server.listen( opts.port || null );
+
+let brown = '\033[33m',
+	green = '\033[32m',
+	reset = '\033[0m';
+
+let slidesLocation = 'http://localhost' + ( opts.port ? ( ':' + opts.port ) : '' );
+
+console.log( brown + 'reveal.js - Speaker Notes' + reset );
+console.log( '1. Open the slides at ' + green + slidesLocation + reset );
+console.log( '2. Click on the link in your JS console to go to the notes page' );
+console.log( '3. Advance through your slides and your notes will advance automatically' );
